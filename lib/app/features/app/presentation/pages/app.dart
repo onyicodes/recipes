779075cf27937dedea09b91450b2c52ea96d3580
Onyedikareal <frankonyeka25@gmail.com>
2023-04-13 23:49:14 +0100

    update: setup app room with getmaterial and easy_localization package

---
+++ lib/app/features/app/presentation/pages/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:recipes/app/routes/app_pages.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.initial,
+      defaultTransition: Transition.fade,
+      locale:  Locale('en', 'US'),
+    );
+  }
+}

6bd087edf43bc6c768d5b44f700151b4027b0a6a
Onyedikareal <frankonyeka25@gmail.com>
2023-04-14 14:26:09 +0100

    update: added seperator builder to listview builder

--- lib/app/features/home/presentation/widgets/ingredient_builder.dart
+++ lib/app/features/home/presentation/widgets/ingredient_builder.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:recipes/app/features/home/presentation/controllers/home_controller.dart';
 import 'package:recipes/app/features/home/presentation/widgets/ingredient_card.dart';
+import 'package:recipes/core/constants/general_constants.dart';
+import 'package:recipes/core/general_widgets/custom_list_space.dart';
 
 class IngredientBuilder extends StatelessWidget {
   const IngredientBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  GetX<HomeController>(
-                builder: (_) {
-                  return ListView.builder(itemBuilder: (context, index) {
-                    return IngredientCard(
-                        selected: _.selectedIngredient
-                            .contains(_.ingredients[index]),
-                        ingredientTitle: _.ingredients[index]);
-                  });
-                },
-              );
+    return GetX<HomeController>(
+      builder: (_) {
+        return ListView.separated(
+          itemCount: _.ingredients.length,
+          separatorBuilder: ((context, index) => CustomListSpacing(
+              spacingValue: ListSpacingValue.spacingV16.value)
+        ), itemBuilder: (context, index) {
+          return IngredientCard(
+              selected: _.selectedIngredient.contains(_.ingredients[index]),
+              ingredientTitle: _.ingredients[index]);
+        });
+      },
+    );
   }
 }
--- lib/app/features/home/presentation/widgets/recipes_builder.dart
+++ lib/app/features/home/presentation/widgets/recipes_builder.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/app/features/home/presentation/widgets/recipes_card.dart';
+import 'package:recipes/core/constants/general_constants.dart';
+import 'package:recipes/core/general_widgets/custom_list_space.dart';
 
 class RecipesBuilder extends StatelessWidget {
   final List recipes;
@@ -7,8 +9,11 @@ class RecipesBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
         itemCount: recipes.length,
+        separatorBuilder: ((context, index) => CustomListSpacing(
+              spacingValue: ListSpacingValue.spacingV16.value)
+        ),
         itemBuilder: (context, index) {
           return RecipesCard(
       recipe: recipes[index]);
